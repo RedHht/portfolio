4c3446525639581a07f0d1e109caa63d4667bcab
redhht <sniper7392018@gmail.com>
2024-03-30 23:27:45 -0300

    Added new technologies

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,7 +1,6 @@
 let burguer = document.getElementById("burguer-button");
 let mobileNav = document.getElementById("mobile-nav");
 let navv = document.getElementById("nav-bar");
-let navColor = "bg-red-400"
 
 burguer.addEventListener("click", toggleNav);
 
@@ -9,11 +8,11 @@ burguer.addEventListener("click", toggleNav);
 function toggleNav() {
     if (mobileNav.classList.contains("hidden")) {
         mobileNav.classList.remove("hidden");
-        navv.style.backgroundColor = "red"
+        navv.classList.add("bg-red-900");
         burguer.innerHTML = "<i class=\"fas fa-times text-white\"></i>";
     } else {
         mobileNav.classList.add("hidden");
-        navv.style.backgroundColor = "transparent"
+        navv.classList.remove("bg-red-900");
         burguer.innerHTML = "<i class=\"fas fa-bars text-white\"></i>";
     }
 }
