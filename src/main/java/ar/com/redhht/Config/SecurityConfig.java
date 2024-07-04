6684b55242ac8938fbc1a9b3ff03990631a44763
redhht <sniper7392018@gmail.com>
2024-07-04 14:43:40 -0300

    Fixed mobile navbar not showing correctly sometimes.

--- src/main/java/ar/com/redhht/Config/SecurityConfig.java
+++ src/main/java/ar/com/redhht/Config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
