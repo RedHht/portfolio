c143a995dd3c1445dc28047fbdb7b882e124a16c
redhht <sniper7392018@gmail.com>
2024-07-15 15:53:06 -0300

    Added login/logout buttons.

--- src/main/java/ar/com/redhht/Config/SecurityConfig.java
+++ src/main/java/ar/com/redhht/Config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 
 @Configuration
