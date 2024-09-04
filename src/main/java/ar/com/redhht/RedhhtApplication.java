c60fe74313755b352faf1ae0da68e21bb89821a0
Thiago Nicolas <sniper7392018@gmail.com>
2024-09-04 15:50:01 -0300

    Some icons deleted

--- src/main/java/ar/com/redhht/RedhhtApplication.java
+++ src/main/java/ar/com/redhht/RedhhtApplication.java
@@ -1,15 +1,7 @@
 package ar.com.redhht;
 
-import ar.com.redhht.Model.Entity.DbUser;
-import ar.com.redhht.Model.Table.UserRepository;
-import org.apache.catalina.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.File;
 
 @SpringBootApplication
 public class RedhhtApplication {
