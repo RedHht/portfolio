f23d6117eaa76189f9c717d44c81bf4d7223a06f
redhht <thiagoianuzzi@protonmail.com>
2024-12-25 18:14:39 -0300

    Pagination of BlogController enhanced by adding "first, previous, next, last" page buttons.

--- src/main/java/ar/com/redhht/Controller/BlogController.java
+++ src/main/java/ar/com/redhht/Controller/BlogController.java
@@ -3,7 +3,6 @@ package ar.com.redhht.Controller;
 import ar.com.redhht.Model.Entity.Post;
 import ar.com.redhht.Model.Table.PostRepository;
 import ar.com.redhht.Model.Table.UserRepository;
-import jakarta.servlet.ServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -32,21 +31,19 @@ public class BlogController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping(path = {""})
+    @GetMapping("")
     public String handle(Model model, @RequestParam(required = false) Integer page) {
-        if (page == null)
-            page = Integer.valueOf(0);
-        Page<Post> posts = this.postRepository.findAll(Pageable.ofSize(10).withPage(page.intValue()));
-        model.addAttribute("posts", posts);
-        model.addAttribute("pageNumber", page);
+        if (page == null) page = 0;
+        Page<Post> postPage = this.postRepository.findAll(Pageable.ofSize(10).withPage(page));
+        model.addAttribute("postPage", postPage);
         model.addAttribute("page", "blog");
         model.addAttribute("title", "Blog");
         return "blog/index";
     }
 
     @GetMapping(path = {"/view/{postId}"})
-    public String view(Model model, @PathVariable int postId) {
-        Post post = this.postRepository.getPostById(Long.valueOf(postId));
+    public String view(Model model, @PathVariable Long postId) {
+        Post post = this.postRepository.getPostById(postId);
         if (post == null)
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El post no existe");
         model.addAttribute("post", post);
@@ -56,7 +53,7 @@ public class BlogController {
     }
 
     @GetMapping(path = {"/new"})
-    public String newPost(Model model, Authentication authentication, ServletRequest servletRequest) {
+    public String newPost(Model model, Authentication authentication) {
         model.addAttribute("page", "blog");
         model.addAttribute("title", "Blog - Nuevo post ");
         if (authentication == null)
@@ -78,23 +75,26 @@ public class BlogController {
     }
 
     @GetMapping(path = {"/edit/{postId}"})
-    public String editPost(Model model, @PathVariable int postId, Authentication authentication) {
-        Post post = this.postRepository.getPostById(Long.valueOf(postId));
-        if (!post.getUser().getUser().equals(authentication.getName()))
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No puedes editar este post.");
+    public String editPost(Model model, @PathVariable Long postId, Authentication authentication) {
+        Post post = this.postRepository.getPostById(postId);
         if (post == null)
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El post no existe");
+        if (!post.getUser().getUser().equals(authentication.getName()))
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No puedes editar este post.");
         model.addAttribute(post);
         return "blog/edit";
     }
 
     @PostMapping(path = {"/edit/{postId}"})
-    public String editPostForm(WebRequest webRequest, @PathVariable int postId, Authentication authentication) {
-        Post originalPost = this.postRepository.getPostById(Long.valueOf(postId));
-        if (!originalPost.getUser().getUser().equals(authentication.getName()))
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No puedes editar este post.");
+    public String editPostForm(WebRequest webRequest, @PathVariable Long postId, Authentication authentication) {
+        Post originalPost = this.postRepository.getPostById(postId);
+
         if (originalPost == null)
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El post no existe");
+
+        if (!originalPost.getUser().getUser().equals(authentication.getName()))
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No puedes editar este post.");
+
         originalPost.setBody(webRequest.getParameter("body"));
         originalPost.setTitle(webRequest.getParameter("title"));
         this.postRepository.save(originalPost);
@@ -102,12 +102,12 @@ public class BlogController {
     }
 
     @GetMapping(path = {"/delete/{postId}"})
-    public String deletePostForm(@PathVariable int postId, Authentication authentication) {
-        Post originalPost = this.postRepository.getPostById(Long.valueOf(postId));
-        if (!originalPost.getUser().getUser().equals(authentication.getName()))
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No puedes eliminar este post.");
+    public String deletePostForm(@PathVariable Long postId, Authentication authentication) {
+        Post originalPost = this.postRepository.getPostById(postId);
         if (originalPost == null)
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "El post no existe");
+        if (!originalPost.getUser().getUser().equals(authentication.getName()))
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "No puedes eliminar este post.");
         this.postRepository.delete(originalPost);
         return "redirect:/blog";
     }
